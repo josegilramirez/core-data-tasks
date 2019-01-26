14156053e4d1d68d796c4afe4aecb91be6a39982
José Gil Ramírez Sánchez <josegil_ramirez@sps.com.mx>
2019-01-25 20:11:31 -0600

    implementing core data

---
+++ Tasks Core Data/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Tasks Core Data
+//
+//  Created by José Gil Ramírez S on 1/25/19.
+//  Copyright © 2019 José Gil Ramírez S. All rights reserved.
+//
+
+import UIKit
+import CoreData  //Para usar las funciones de CoreData
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    // var tasks: [String] = [] // originalmente era string y luego cambiamos a NSManagedObjects con CoreDate
+    var tasks: [NSManagedObject] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "The List"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+
+    @IBAction func addTaskName(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "New Task", message: "Add a new task", preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) {
+            [unowned self] action in
+                guard
+                    let textField = alert.textFields?.first,
+                    let nameToSave = textField.text
+                    else { return }
+            
+            self.tasks.append(nameToSave)
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+    
+    func save(name: String) {
+        // Nos traemos el singleton del AppDelegate para acceder al PersistenContainer
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // Mandamos llamar la entidad Task que tenemos en el modelo Tasks_Core_Data.xcdatamodelid
+        let entity = NSEntityDescription.entity(forEntityName: "Task", in: managedContext)
+        
+    }
+    
+}
+
+// MARK: UITableViewDataSource
+    
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        // cell.textLabel?.text = tasks[indexPath.row] // Al cambiar a NSManagedObject debemos actualizar el acceso
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.value(forKeyPath: "name") as? String
+        return cell
+    }
+    
+    
+}
+
---
+++ Tasks Core DataTests/Tasks_Core_DataTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tasks_Core_DataTests.swift
+//  Tasks Core DataTests
+//
+//  Created by José Gil Ramírez S on 1/25/19.
+//  Copyright © 2019 José Gil Ramírez S. All rights reserved.
+//
+
+import XCTest
+@testable import Tasks_Core_Data
+
+class Tasks_Core_DataTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
